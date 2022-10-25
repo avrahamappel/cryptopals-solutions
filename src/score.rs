0f1cb44905713c4e376de90c3bc7002412a6899e
Avraham Appel <aappel248@gmail.com>
2022-10-25 10:37:43 -0400

    A new approach

--- src/score.rs
+++ src/score.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
+use std::ops::Sub;
 
-const SAMPLE_TEXT: &str = r#"
+const SAMPLE_TEXT: &[u8; 459] = br#"
     Single-byte cipher
 
     The hex encoded string has been XOR'd against a single character. Find the key, decrypt the message.
@@ -13,8 +14,8 @@ const SAMPLE_TEXT: &str = r#"
     Achievement Unlocked
     You now have our permission to make jokes on Twitter."#;
 
-fn char_frequency(text: &str) -> HashMap<u8, f64> {
-    text.bytes()
+fn char_frequency(text: &[u8]) -> HashMap<u8, f64> {
+    text.iter()
         .fold(HashMap::new(), |mut hash, byte| {
             hash.entry(byte)
                 .and_modify(|val| {
@@ -26,7 +27,62 @@ fn char_frequency(text: &str) -> HashMap<u8, f64> {
         .into_iter()
         .map(|(byte, count)| {
             let percent = (count * text.len()) as f64 / 100.0;
-            (byte, percent)
+            (*byte, percent)
         })
         .collect()
+}
+
+fn variances(bytes: &[u8]) -> Vec<f64> {
+    // TODO cache this at compile time
+    let base = char_frequency(SAMPLE_TEXT);
+    let frequencies = char_frequency(bytes);
+
+    frequencies
+        .into_iter()
+        .map(|(b, freq)| {
+            let base_freq = base
+                .get(&b)
+                .expect("base frequencies should contain all chars");
+
+            base_freq.sub(freq).abs()
+        })
+        .collect()
+}
+
+fn average(scores: Vec<f64>) -> f64 {
+    scores.iter().sum::<f64>() / scores.len() as f64
+}
+
+/// The score of a byte string. The lower the number, the more likely it is that the string is
+/// English text.
+pub fn score(bytes: &[u8]) -> f64 {
+    average(variances(bytes))
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn real_text_has_decent_score() {
+        assert_eq!(75.0, super::score(b"Hello world!"));
+    }
+
+    #[test]
+    fn uppercase_etoain_has_perfect_score() {
+        assert_eq!(100.0, super::score(b"ETOAINSHRDLU"));
+    }
+
+    #[test]
+    fn lowercase_etoain_has_perfect_score() {
+        assert_eq!(100.0, super::score(b"etoainshrdlu"));
+    }
+
+    #[test]
+    fn non_alphanumeric_gibberish_has_terrible_score() {
+        assert_eq!(0.0, super::score(b"@%##%#@^^&%$"));
+    }
+
+    #[test]
+    fn alphanumeric_gibberish_has_mediocre_score() {
+        assert_eq!(50.0, super::score(b"djb2iu3h2hfffnc"));
+    }
 }
--- src/xor.rs
+++ src/xor.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use crate::score;
 
 pub fn fixed(first: &[u8], second: &[u8]) -> Option<Vec<u8>> {
     if first.len() != second.len() {
@@ -14,8 +14,6 @@ pub fn fixed(first: &[u8], second: &[u8]) -> Option<Vec<u8>> {
     Some(xord)
 }
 
-const ETAOIN: &[u8; 95] = b" etaniosrldhc\numpfg.ybw,v0k1STC285A9x3I-647MB\"'PENFRDUqLGJHOWjz/<>K)(VY:QZX;?\x7F^&+[]$!*=~_\t{@\x05\x1B\x1E";
-
 /// A single-byte XOR possiblity, with score
 pub struct SingleByteXor {
     pub byte: u8,
@@ -23,64 +21,10 @@ pub struct SingleByteXor {
     pub message: Vec<u8>,
 }
 
-/// How many times does each ETAOIN byte occur in this string
-fn occurences(bytes: &[u8]) -> Vec<(u8, usize)> {
-    let mut occurences = ETAOIN
-        .iter()
-        .map(|b| {
-            let occurences = bytes.iter().filter(|b1| **b1 == *b).count();
-
-            (*b, occurences)
-        })
-        .collect::<Vec<_>>();
-
-    occurences.sort_by(|a, b| b.1.cmp(&a.1));
-
-    occurences
-}
-
-/// How much does the occurence of each ETAOIN byte deviate from most English text
-fn occurence_weights(bytes: &[u8]) -> HashMap<u8, usize> {
-    occurences(bytes)
-        .into_iter()
-        .enumerate()
-        .map(|(i, (b, _))| (b, i))
-        .collect()
-}
-
-/// Variance of occurences from ETAOIN value
-fn variances(bytes: &[u8]) -> Vec<usize> {
-    ETAOIN
-        .iter()
-        .enumerate()
-        .map(|(i, b)| occurence_weights(bytes).get(b).unwrap().abs_diff(i))
-        .collect()
-}
-
-fn average(scores: Vec<usize>) -> usize {
-    scores.iter().sum::<usize>() / scores.len()
-}
-
-fn percent(number: usize) -> f64 {
-    // (256 * 256 * 100) as f64 /
-    number as f64
-}
-
-fn score(bytes: &[u8]) -> f64 {
-    let percent = percent(average(variances(bytes)));
-    percent
-
-    //     if percent > 100.0 {
-    //         0.0
-    //     } else {
-    //         100.0 - percent
-    //     }
-}
-
 pub fn single(bytes: &[u8]) -> impl Iterator<Item = SingleByteXor> + '_ {
     (0x00..=0xFF).map(|b| {
         let message = fixed(bytes, &vec![b; bytes.len()]).unwrap();
-        let score = score(&message);
+        let score = score::score(&message);
 
         SingleByteXor {
             byte: b,
@@ -88,32 +32,4 @@ pub fn single(bytes: &[u8]) -> impl Iterator<Item = SingleByteXor> + '_ {
             score,
         }
     })
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn real_text_has_decent_score() {
-        assert_eq!(75.0, super::score(b"Hello world!"));
-    }
-
-    #[test]
-    fn uppercase_etoain_has_perfect_score() {
-        assert_eq!(100.0, super::score(b"ETOAINSHRDLU"));
-    }
-
-    #[test]
-    fn lowercase_etoain_has_perfect_score() {
-        assert_eq!(100.0, super::score(b"etoainshrdlu"));
-    }
-
-    #[test]
-    fn non_alphanumeric_gibberish_has_terrible_score() {
-        assert_eq!(0.0, super::score(b"@%##%#@^^&%$"));
-    }
-
-    #[test]
-    fn alphanumeric_gibberish_has_mediocre_score() {
-        assert_eq!(50.0, super::score(b"djb2iu3h2hfffnc"));
-    }
 }
