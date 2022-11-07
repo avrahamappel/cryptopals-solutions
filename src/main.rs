bcd0b3cec4c5fdf571b6e93dbf87d88c8c6d3712
Avraham Appel <aappel248@gmail.com>
2022-11-07 11:35:46 -0500

    First pass at exercise 4
    Very slow. Maybe I can use `cargo flamegraph` to find the bottleneck(s)
    Also, the scoring algorithm still needs improvement.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// use `cryptopals::{hex, xor};
+use cryptopals::{hex, xor};
 
 fn main() {
     // Detect single-character XOR
@@ -7,4 +7,28 @@ fn main() {
     // Find it.
 
     // (Your code from #3 should help.)
+    let strings = include_str!("../data/4.txt");
+
+    let mut results = strings
+        .lines()
+        .flat_map(|l| {
+            let bytes = hex::decode(l.trim());
+
+            xor::single(&bytes)
+                .into_iter()
+                .map(|res| (l.to_owned(), res))
+        })
+        .collect::<Vec<_>>();
+
+    results.sort_by(|a, b| a.1.score.total_cmp(&b.1.score));
+
+    for (l, res) in results.iter().take(10) {
+        println!("Hex: {}", l);
+        println!(
+            "Decode by '{}' (probability: {}%): {}",
+            char::from(res.byte),
+            res.score,
+            String::from_utf8_lossy(&res.message)
+        );
+    }
 }
