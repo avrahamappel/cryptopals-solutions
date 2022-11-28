5bc71c220a66b786ddf0e9b7be9de56f69323b3c
Avraham Appel <aappel248@gmail.com>
2022-11-28 13:03:31 -0500

    Cleanup and refactor tests

--- src/score.rs
+++ src/score.rs
@@ -1,5 +1,3 @@
-use integer_hasher::IntMap as HashMap;
-
 use lazy_static::lazy_static;
 
 // This is the text of the challenge
@@ -18,47 +16,14 @@ const SAMPLE_TEXT: &str = r#"
     You now have our permission to make jokes on Twitter."#;
 
 lazy_static! {
-    static ref BASE_FREQUENCIES: HashMap<u8, f64> = char_frequency(SAMPLE_TEXT.as_bytes());
-    static ref BASE_SCORES: Score = Score::from(SAMPLE_TEXT.as_bytes());
+    static ref BASE_SCORE: Score = Score::from(SAMPLE_TEXT.as_bytes());
 }
 
 fn round_to_2(n: f64) -> f64 {
     (n * 100.0).round() / 100.0
 }
 
-fn ratio_to_percent(num: usize, den: usize) -> f64 {
-    round_to_2((num * 100) as f64 / den as f64)
-}
-
-fn char_frequency(text: &[u8]) -> HashMap<u8, f64> {
-    text.iter()
-        .fold(HashMap::default(), |mut hash, byte| {
-            let key = match byte {
-                b'A'..=b'Z' | b'a'..=b'z' => byte.to_ascii_lowercase(),
-                // 'P' for punctuation
-                b',' | b'.' | b'!' | b'?' | b'\'' | b'"' | b'(' | b')' | b'-' => b'P',
-                // 'S' for whitespace
-                b' ' | b'\n' => b'S',
-                // Other symbols
-                _ => b'@',
-            };
-
-            hash.entry(key)
-                .and_modify(|val| {
-                    *val += 1;
-                })
-                .or_insert(1);
-            hash
-        })
-        .into_iter()
-        .map(|(byte, count)| {
-            let percent = ratio_to_percent(count, text.len());
-            (byte, percent)
-        })
-        .collect()
-}
-
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 struct Score {
     avg_word_length: f64,
     avg_vowel_count: f64,
@@ -117,12 +82,12 @@ fn average(values: &[f64]) -> f64 {
 /// The score of a byte string. The lower the number, the
 /// more likely it is that the string is English text.
 pub(crate) fn score(bytes: &[u8]) -> f64 {
-    round_to_2(Score::from(bytes).diff(*BASE_SCORES).avg())
+    round_to_2(Score::from(bytes).diff(*BASE_SCORE).avg())
 }
 
 #[cfg(test)]
 mod tests {
-    use super::SAMPLE_TEXT;
+    use super::*;
 
     // From https://randomwordgenerator.com/sentence.php
     const REAL_TEXT: &str = "She felt that chill that makes the hairs on the back of your neck when he walked into the room.";
@@ -130,71 +95,43 @@ mod tests {
     const SOME_BYTES: &str = "6^*&^*^&%#";
 
     #[test]
-    fn ratio_to_percent() {
-        assert_eq!(20.0, super::ratio_to_percent(1, 5));
-    }
-
-    #[test]
-    fn char_frequency() {
+    fn base_score() {
         for (input, expected) in [
             (
                 SAMPLE_TEXT,
-                &[
-                    (b'S', 22.87),
-                    (b'e', 10.81),
-                    (b'o', 7.07),
-                    (b't', 6.86),
-                    (b'i', 4.78),
-                    (b'n', 4.78),
-                    (b'a', 4.37),
-                    (b'h', 4.16),
-                    (b's', 3.95),
-                    (b'c', 3.74),
-                    (b'r', 3.74),
-                    (b'P', 3.53),
-                    (b'd', 3.33),
-                    (b'u', 2.08),
-                    (b'g', 1.66),
-                    (b'm', 1.66),
-                    (b'y', 1.66),
-                    (b'p', 1.46),
-                    (b'l', 1.25),
-                    (b'b', 1.04),
-                    (b'f', 1.04),
-                    (b'w', 1.04),
-                    (b'k', 0.83),
-                    (b'v', 0.83),
-                    (b'x', 0.62),
-                    (b'j', 0.21),
-                    (b'q', 0.21),
-                    (b'z', 0.21),
-                    (b'@', 0.21),
-                ][..],
+                Score {
+                    avg_word_length: 3.82,
+                    avg_vowel_count: 1.4,
+                    avg_consonant_count: 2.13,
+                },
             ),
             (
                 REAL_TEXT,
-                &[
-                    (b'S', 20.00),
-                    (b'h', 10.53),
-                    (b'e', 10.53),
-                    (b'o', 6.32),
-                    (b'l', 4.21),
-                    (b'r', 3.16),
-                    (b'w', 2.11),
-                    (b'd', 1.05),
-                    (b'P', 1.05),
-                ][..],
+                Score {
+                    avg_word_length: 3.8,
+                    avg_vowel_count: 1.3,
+                    avg_consonant_count: 2.45,
+                },
             ),
             (
                 GIBBERISH,
-                &[(b'a', 16.67), (b'e', 10.0), (b'f', 10.0), (b'j', 6.67)][..],
+                Score {
+                    avg_word_length: 9.33,
+                    avg_vowel_count: 2.67,
+                    avg_consonant_count: 6.67,
+                },
             ),
-            (SOME_BYTES, &[(b'@', 100.0)][..]),
+            (
+                SOME_BYTES,
+                Score {
+                    avg_word_length: 10.0,
+                    avg_vowel_count: 0.0,
+                    avg_consonant_count: 0.0,
+                },
+            ),
         ] {
-            let frequencies = super::char_frequency(input.as_bytes());
-            for (byte, frequency) in expected {
-                assert_eq!((byte, frequency), frequencies.get_key_value(byte).unwrap());
-            }
+            let frequencies = Score::from(input.as_bytes());
+            assert_eq!(expected, frequencies);
         }
     }
 
