107743887e69e742c461c3a691075b7608ea2553
Avraham Appel <aappel248@gmail.com>
2022-11-28 00:00:11 -0500

    workin but broke previous feature test

--- src/score.rs
+++ src/score.rs
@@ -1,5 +1,4 @@
 use integer_hasher::IntMap as HashMap;
-use std::ops::Sub;
 
 use lazy_static::lazy_static;
 
@@ -20,7 +19,7 @@ const SAMPLE_TEXT: &str = r#"
 
 lazy_static! {
     static ref BASE_FREQUENCIES: HashMap<u8, f64> = char_frequency(SAMPLE_TEXT.as_bytes());
-    static ref BASE_WORD_LENGTH: f64 = avg_word_length(SAMPLE_TEXT.as_bytes());
+    static ref BASE_SCORES: Score = Score::from(SAMPLE_TEXT.as_bytes());
 }
 
 fn round_to_2(n: f64) -> f64 {
@@ -59,35 +58,66 @@ fn char_frequency(text: &[u8]) -> HashMap<u8, f64> {
         .collect()
 }
 
-fn avg_word_length(bytes: &[u8]) -> f64 {
-    average(
-        bytes
+#[derive(Clone, Copy)]
+struct Score {
+    avg_word_length: f64,
+    avg_vowel_count: f64,
+    avg_consonant_count: f64,
+}
+
+impl From<&[u8]> for Score {
+    fn from(bytes: &[u8]) -> Self {
+        let (awl, (avc, acc)): (Vec<_>, (Vec<_>, Vec<_>)) = bytes
             .split(|b| *b == b' ')
-            .map(|w| w.len() as f64)
-            .collect(),
-    )
+            .map(|w| {
+                let word_length = w.len();
+                let lower = w.to_ascii_lowercase();
+                let vowels = lower.iter().filter(|b| b"aeiou".contains(b)).count();
+                let consonants = lower
+                    .iter()
+                    .filter(|b| b"bcdfghjklmnpqrstvwxyz".contains(b))
+                    .count();
+                (word_length as f64, (vowels as f64, consonants as f64))
+            })
+            .unzip();
+
+        Self {
+            avg_word_length: average(&awl),
+            avg_vowel_count: average(&avc),
+            avg_consonant_count: average(&acc),
+        }
+    }
 }
 
-#[allow(unused)]
-fn variances(bytes: &[u8]) -> Vec<f64> {
-    char_frequency(bytes)
-        .into_iter()
-        .map(|(b, freq)| {
-            let base_freq = BASE_FREQUENCIES.get(&b).unwrap_or(&0.0);
+impl Score {
+    fn diff(self, other: Self) -> Self {
+        Self {
+            avg_word_length: (self.avg_word_length - other.avg_word_length).abs(),
+            avg_vowel_count: (self.avg_vowel_count - other.avg_vowel_count).abs(),
+            avg_consonant_count: (self.avg_consonant_count - other.avg_consonant_count).abs(),
+        }
+    }
 
-            round_to_2(base_freq.sub(freq).abs())
-        })
-        .collect()
+    fn avg(self) -> f64 {
+        average(
+            [
+                self.avg_word_length,
+                self.avg_vowel_count,
+                self.avg_consonant_count,
+            ]
+            .as_slice(),
+        )
+    }
 }
 
-fn average(scores: Vec<f64>) -> f64 {
-    round_to_2(scores.iter().sum::<f64>() / scores.len() as f64)
+fn average(values: &[f64]) -> f64 {
+    round_to_2(values.iter().sum::<f64>() / values.len() as f64)
 }
 
 /// The score of a byte string. The lower the number, the
 /// more likely it is that the string is English text.
 pub fn score(bytes: &[u8]) -> f64 {
-    round_to_2(avg_word_length(bytes).sub(*BASE_WORD_LENGTH).abs())
+    round_to_2(Score::from(bytes).diff(*BASE_SCORES).avg())
 }
 
 #[cfg(test)]
@@ -172,9 +202,10 @@ mod tests {
     fn score() {
         for (input, expected) in [
             (SAMPLE_TEXT, 0.0),
-            (REAL_TEXT, 0.02),
-            (GIBBERISH, 5.51),
-            (SOME_BYTES, 6.18),
+            (REAL_TEXT, 0.15),
+            (GIBBERISH, 3.77),
+            // Have to record this kind
+            (SOME_BYTES, 3.24),
         ] {
             assert_eq!(expected, super::score(input.as_bytes()));
         }
