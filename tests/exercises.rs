de5c1bbd2f7e1c569af45d2873e2b2120daf0e3c
Avraham Appel <aappel248@gmail.com>
2022-12-21 22:35:34 -0500

    Add comment about slow test in debug

--- tests/exercises.rs
+++ tests/exercises.rs
@@ -43,6 +43,9 @@ fn single_byte_xor() {
     );
 }
 
+/// This test takes a long time when compiled in debug mode. I think the sorting
+/// slows it down. If it starts slowing down in release mode, I'll look
+/// into using BTreeSets or something instead of sorted Vecs.
 #[test]
 fn detect_single_char_xor() {
     let strings = include_str!("../data/4.txt");
