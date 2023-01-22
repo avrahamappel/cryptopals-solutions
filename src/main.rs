323b9e6d79411d35e04f96addddec3d9f639c27d
Avraham Appel <aappel248@gmail.com>
2023-01-22 15:31:25 -0500

    Next challenge

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
-use cryptopals::hex;
-use cryptopals::xor::repeating;
+// use cryptopals::hex;
+// use cryptopals::xor::repeating;
 
 fn main() {
-    // Implement repeating-key XOR
-    // Here is the opening stanza of an important work of the English language:
+    // There's a file here. It's been base64'd after being encrypted with repeating-key XOR.
 
-    let input = "
-Burning 'em, if you ain't quick and nimble
-I go crazy when I hear a cymbal
-"
-    // Encrypt it, under the key "ICE", using repeating-key XOR.
-    .trim()
-    .as_bytes();
+    // Decrypt it.
 
-    let encrypted = repeating(input, "ICE".as_bytes());
+    // Here's how:
 
-    // In repeating-key XOR, you'll sequentially apply each byte of the key; the first byte of plaintext will be XOR'd against I, the next C, the next E, then I again for the 4th byte, and so on.
-
-    // It should come out to:
-
-    // 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
-    // a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
-    println!("{}", hex::encode(&encrypted));
-
-    // Encrypt a bunch of stuff using your repeating-key XOR function. Encrypt your mail. Encrypt your password file. Your .sig file. Get a feel for it. I promise, we aren't wasting your time with this.
+    // Let KEYSIZE be the guessed length of the key; try values from 2 to (say) 40.
+    // Write a function to compute the edit distance/Hamming distance between two strings. The Hamming distance is just the number of differing bits. The distance between:
+    // this is a test
+    // and
+    // wokka wokka!!!
+    // is 37. Make sure your code agrees before you proceed.
+    // For each KEYSIZE, take the first KEYSIZE worth of bytes, and the second KEYSIZE worth of bytes, and find the edit distance between them. Normalize this result by dividing by KEYSIZE.
+    // The KEYSIZE with the smallest normalized edit distance is probably the key. You could proceed perhaps with the smallest 2-3 KEYSIZE values. Or take 4 KEYSIZE blocks instead of 2 and average the distances.
+    // Now that you probably know the KEYSIZE: break the ciphertext into blocks of KEYSIZE length.
+    // Now transpose the blocks: make a block that is the first byte of every block, and a block that is the second byte of every block, and so on.
+    // Solve each block as if it was single-character XOR. You already have code to do this.
+    // For each block, the single-byte XOR key that produces the best looking histogram is the repeating-key XOR key byte for that block. Put them together and you have the key.
+    // This code is going to turn out to be surprisingly useful later on. Breaking repeating-key XOR ("Vigenere") statistically is obviously an academic exercise, a "Crypto 101" thing. But more people "know how" to break it than can actually break it, and a similar technique breaks something much more important.
 }
--- tests/exercises.rs
+++ tests/exercises.rs
@@ -69,3 +69,17 @@ fn detect_single_char_xor() {
         String::from_utf8_lossy(&result.message).trim()
     );
 }
+
+#[test]
+fn repeating_key_xor() {
+    let input = "
+Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal
+"
+    .trim()
+    .as_bytes();
+
+    let encrypted = xor::repeating(input, "ICE".as_bytes());
+
+    assert_eq!("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f", hex::encode(&encrypted));
+}
