6765d9c1249845f7c6221ab4ce9edd35b7325ba2
Avraham Appel <aappel248@gmail.com>
2023-01-22 23:27:59 -0500

    Implement base64 decode

--- src/base64.rs
+++ src/base64.rs
@@ -5,44 +5,34 @@ const UPPERCASEOFFSET: u8 = 65;
 const LOWERCASEOFFSET: u8 = 71;
 const DIGITOFFSET: u8 = 4;
 
-struct Alphabet;
+fn get_char_for_index(index: u8) -> Option<char> {
+    let char = match index {
+        0..=25 => index + UPPERCASEOFFSET,
+        26..=51 => index + LOWERCASEOFFSET,
+        52..=61 => index - DIGITOFFSET,
+        62 => 43,
+        63 => 47,
+        _ => return None,
+    };
 
-impl Alphabet {
-    fn get_char_for_index(index: u8) -> Option<char> {
-        let char = match index {
-            0..=25 => index + UPPERCASEOFFSET,
-            26..=51 => index + LOWERCASEOFFSET,
-            52..=61 => index - DIGITOFFSET,
-            62 => 43,
-            63 => 47,
-            _ => return None,
-        };
+    Some(char.into())
+}
 
-        Some(char.into())
-    }
+fn get_index_for_char(char: u8) -> Option<u8> {
+    let index = match char {
+        65..=90 => char - UPPERCASEOFFSET,  // A-Z
+        97..=122 => char - LOWERCASEOFFSET, // a-z
+        48..=57 => char + DIGITOFFSET,      // 0-9
+        43 => 62,                           // +
+        47 => 63,                           // /
+        _ => return None,
+    };
 
-    #[allow(unused)]
-    fn get_index_for_char(char: char) -> Option<u8> {
-        if let Ok(char) = char.try_into() {
-            let index = match char {
-                65..=90 => char - UPPERCASEOFFSET,  // A-Z
-                97..=122 => char - LOWERCASEOFFSET, // a-z
-                48..=57 => char + DIGITOFFSET,      // 0-9
-                43 => 62,                           // +
-                47 => 63,                           // /
-                _ => return None,
-            };
+    Some(index)
+}
 
-            return Some(index);
-        }
-
-        None
-    }
-
-    #[allow(unused)]
-    fn get_padding_char() -> char {
-        '='
-    }
+const fn get_padding_char() -> u8 {
+    b'='
 }
 
 fn split(chunk: &[u8]) -> Vec<u8> {
@@ -66,11 +56,51 @@ fn split(chunk: &[u8]) -> Vec<u8> {
     }
 }
 
+fn stitch(bytes: &[u8]) -> Vec<u8> {
+    let out = match bytes.len() {
+        2 => vec![
+            (bytes[0] & 0b00111111) << 2 | bytes[1] >> 4,
+            (bytes[1] & 0b00001111) << 4,
+        ],
+
+        3 => vec![
+            (bytes[0] & 0b00111111) << 2 | bytes[1] >> 4,
+            (bytes[1] & 0b00001111) << 4 | bytes[2] >> 2,
+            (bytes[2] & 0b00000011) << 6,
+        ],
+
+        4 => vec![
+            (bytes[0] & 0b00111111) << 2 | bytes[1] >> 4,
+            (bytes[1] & 0b00001111) << 4 | bytes[2] >> 2,
+            (bytes[2] & 0b00000011) << 6 | bytes[3] & 0b00111111,
+        ],
+
+        _ => unreachable!(),
+    };
+
+    out.into_iter().filter(|&x| x > 0).collect()
+}
+
 pub fn encode(bytes: &[u8]) -> String {
     bytes
         .chunks(3)
         .flat_map(split)
-        .filter_map(Alphabet::get_char_for_index)
+        .filter_map(get_char_for_index)
+        .collect()
+}
+
+pub fn decode(string: &str) -> Vec<u8> {
+    string
+        .as_bytes()
+        .chunks_exact(4)
+        .map(|chunk| {
+            chunk
+                .iter()
+                .filter(|b| **b != get_padding_char())
+                .filter_map(|b| get_index_for_char(*b))
+                .collect::<Vec<_>>()
+        })
+        .flat_map(|chars| stitch(&chars))
         .collect()
 }
 
@@ -89,5 +119,24 @@ mod tests {
     #[test]
     fn encode() {
         assert_eq!("UGFuY2FrZQ", super::encode("Pancake".as_bytes()));
+    }
+
+    // The binary representation of "Hi" is `01001000 01101001 00100001`
+    #[test_case(&[0b00010010, 0b00000000], b"H".to_vec(); "One char")]
+    #[test_case(&[0b00010010, 0b00000110, 0b00100100], b"Hi".to_vec(); "Two chars")]
+    #[test_case(&[0b00010010, 0b00000110, 0b00100100, 0b00100001], b"Hi!".to_vec(); "Three chars")]
+    fn stitch(input: &[u8], expected: Vec<u8>) {
+        assert_eq!(expected, super::stitch(input));
+    }
+
+    #[test]
+    fn decode() {
+        assert_eq!(
+            "Pancake",
+            super::decode("UGFuY2FrZQ==")
+                .into_iter()
+                .map(char::from)
+                .collect::<String>()
+        );
     }
 }
