579fa5fd868632a4cbc2f22e8cc79879072e3b1b
Avraham Appel <aappel248@gmail.com>
2023-02-05 23:33:22 -0500

    Move to xor module

--- src/xor.rs
+++ src/xor.rs
@@ -1,3 +1,4 @@
+use crate::hamming;
 use crate::score;
 use crate::sorted::Sorted;
 
@@ -69,4 +70,96 @@ pub fn single(bytes: &[u8]) -> Vec<SingleByteXor> {
         })
         .collect::<Vec<_>>()
         .sorted()
+}
+
+/// Transpose blocks of a byte string: construct a list of blocks where the first block is
+/// comprised of the first byte of every block, the second is the second byte of every block, and
+/// so on.
+fn transpose(input: &[u8], size: usize) -> Vec<Vec<u8>> {
+    input.chunks_exact(size).fold(
+        (0..size)
+            .map(|_| Vec::with_capacity(input.len() / size))
+            .collect(),
+        |mut blocks: Vec<_>, chunk| {
+            for i in 0..size {
+                blocks[i].push(chunk[i]);
+            }
+
+            blocks
+        },
+    )
+}
+
+// Return a list of possible sizes of key used to encode the given bytes with
+// repeating-key xor, ranked from most likely to least likely.
+fn guess_keysizes(input: &[u8]) -> Vec<usize> {
+    let mut keysizes: Vec<_> = (2..=40)
+        .map(|keysize| {
+            let chunk1 = &input[..keysize];
+            let chunk2 = &input[keysize..(keysize + 1)];
+
+            let dist: f32 = hamming::distance(chunk1, chunk2) as f32;
+
+            let normalized: f32 = dist / keysize as f32;
+
+            (normalized, keysize)
+        })
+        .collect();
+
+    keysizes.sort_by(|(n1, _), (n2, _)| n1.total_cmp(n2));
+
+    keysizes.into_iter().map(|(_, k)| k).collect()
+}
+
+/// Crack repeating-key xor.
+pub fn repeating_crack(input: &[u8]) {
+    for keysize in guess_keysizes(input).iter() {
+        println!("Keysize: {keysize}");
+
+        let key: Vec<_> = transpose(input, *keysize)
+            .into_iter()
+            .map(|block| {
+                // println!("Block: {}", String::from_utf8_lossy(&block));
+                let res = &single(&block)[0];
+
+                // println!("Possible key:");
+                // println!("Byte: {}", res.byte);
+                // println!("Decoded block: {}", String::from_utf8_lossy(&res.message));
+                // println!("Probability: {}", res.score);
+
+                res.byte
+            })
+            // For each block, the single-byte XOR key that produces the best looking histogram is the repeating-key XOR key byte for that block. Put them together and you have the key.
+            .collect();
+
+        println!("Key: {}", String::from_utf8_lossy(&key));
+
+        let decoded = repeating(input, &key);
+
+        println!("Decoded: {}", String::from_utf8_lossy(&decoded));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_transpose() {
+        let input = b"123456789";
+
+        assert_eq!(
+            vec![
+                vec![b'1', b'4', b'7'],
+                vec![b'2', b'5', b'8'],
+                vec![b'3', b'6', b'9'],
+            ],
+            transpose(input.as_slice(), 3)
+        );
+    }
+
+    #[test]
+    fn test_repeating_crack() {
+        todo!()
+    }
 }
